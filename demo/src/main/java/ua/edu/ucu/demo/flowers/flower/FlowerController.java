30c9cf650b0ed0d64a4d23bbd37cac9abd4fef80
yuliavistak <vistak.pn@ucu.edu.ua>
2023-11-09 22:35:42 +0200

    Fixes

--- demo/src/main/java/ua/edu/ucu/demo/flowers/flower/FlowerController.java
+++ demo/src/main/java/ua/edu/ucu/demo/flowers/flower/FlowerController.java
@@ -6,18 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController // треба шукати url
+@RestController//треба шукати url
 @RequestMapping("/api/flower")
 public class FlowerController {
     @GetMapping("/hello")
-	public String getHello() {
-		return "Hello";
-	}
+    public String getHello() {
+        return "Hello";
+    }
 
     @GetMapping("/list")
-	public List<Flower> getFlower() {
-		return List.of(new Flower());
-		
-	}
-    
+    public List<Flower> getFlower() {
+        return List.of(new Flower());
+    }
 }
